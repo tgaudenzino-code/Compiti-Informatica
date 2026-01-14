707f3fb00b8212d1c6db4551b7a3b6aa6c7e8a0d
Tiziano <t.gaudenzino@gmail.com>
2026-01-14 17:58:14 +0100

    esercizi 19

---
+++ C++/16-12-25/pag118 n19.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    int r;
+    int rmio;
+    int rmiod;
+    cout<<"inserisci i numeri da sommare ";
+    cin>>n1>>n2;
+    cout<<"inserisci il risultato da te ottenuto ";
+    cin>>rmio;
+    r=n1+n2;
+    if (rmio==r)
+    {
+        cout<<"bravo, hai indovinato al primo tentativo";
+    }
+    else
+    {
+        cout<<"hai sbagliato, ritenta sarai più fortunato ";
+        cin>>rmiod;
+        if (rmiod==r)
+        {
+            cout<<"bravino dai";
+        }
+        else
+        {
+            cout<<"...hai sbagliato...di nuovo...";
+        }
+    }
+
+}
+
