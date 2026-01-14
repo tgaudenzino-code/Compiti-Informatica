2cb44c070617ed4de327c07cf4166cdfad372542
Tiziano <t.gaudenzino@gmail.com>
2026-01-14 17:25:58 +0100

    esrcizi 17 18

---
+++ C++/16-12-25/pag118 es 18.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int prezzof;
+    int nl;
+    int settimane;
+    cout<<"quante settimane vuoi soggiornare? una o due? ";
+    cin>>settimane;
+    cout<<"vuoi le lenzuola? 1=si 2=no ";
+    cin>>nl;
+    if (settimane==1)
+    {
+        prezzof=100+600;
+    }
+    else
+    {
+        prezzof=100+1100;
+    }
+    if (settimane==2 && nl==1)
+    {
+        prezzof=prezzof+20*2;
+    }
+    else if (settimane==1 && nl==1)
+    {
+        prezzof=prezzof+20;
+    }
+    cout<<prezzof;
+}
