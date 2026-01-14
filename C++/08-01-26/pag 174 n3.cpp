7577175cd9630eb7223f1631a4e823c42c112b3a
Tiziano <t.gaudenzino@gmail.com>
2026-01-14 19:47:50 +0100

    esercizi 2 3 11

---
+++ C++/08-01-26/pag 174 n3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    cout<<"inserisci il numero di partenza ";
+    int a;
+    cin>>a;
+    cout<<"adesso quello finale ";
+    int b;
+    cin>>b;
+    int c=0;
+       for (int i=a;i<=b;i++)
+    {
+        if (i%2==0)
+        {
+            cout<<i<<" ";
+            c++;
+        }
+    }
+    cout<<"\n"<<"ecco il conteggio dei numeri pari: ";
+    cout<<c;
+}
---
+++ C++/16-01-26/pag 174 n11.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int p;
+    int n;
+    int n1;
+    int n2;
+    cout<<"inserisci quante coppie di numeri vuoi controllare ";
+    cin>>n;
+    for (int i=1;i<=n;i++)
+    {
+        cout<<"inserisci coppia: ";
+        cin>>n1>>n2;
+        if (n1==n2)
+        {
+            p++;
+        }
+    }
+    cout<<"numero di volte numeri uguali: ";
+    cout<<p;
+}
