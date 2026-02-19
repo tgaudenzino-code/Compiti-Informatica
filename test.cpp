39dc50ff4cd229f25713276f5a494728c472bf64
tgaudenzino-code <t.gaudenzino@gmail.com>
2026-02-19 09:43:53 +0100

    salvo

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Includi la libreria per l'input/output
+
+int main() {
+    // std::cout invia il testo allo schermo
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Indica che il programma è terminato con successo
+}
